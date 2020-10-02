d4f953566b41e265ae50cde108c5bb8b7c8da44f
Abhinav Gupta <guptaabhinav707@gmail.com>
2020-10-02 21:26:27 +0530

    hello world program
    this is a simple hello world program

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h> /// header file contain the declaration of the printf function
+int main()
+{
+
+    printf("Hello World!"); /// printf() is a function to print any thing on screen ;
+    return 0; /// to show that something is return to the os
+}
